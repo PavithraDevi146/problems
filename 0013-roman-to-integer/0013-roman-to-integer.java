cff9842363d6ff4041141fd72ecc2d0d1dfc7615
Pavithra Devi <pavithradevim.23csd@kongu.edu>
2026-07-04 09:03:11 +0530

    Time: 2 ms (100%), Space: 46.5 MB (62.61%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int romanToInt(String s) {
+        int total=0;
+        
+        int prev=0;
+        for(int i=s.length()-1;i>=0;i--){
+            int current=0;
+            switch(s.charAt(i)){
+                case 'I':
+                    current=1;
+                    break;
+                case 'V':
+                    current=5;
+                    break;
+                case 'X':
+                    current=10;
+                    break;
+                case 'L':
+                    current=50;
+                    break;
+                case 'C':
+                    current=100;
+                    break;
+                case 'D':
+                    current=500;
+                    break;
+                case 'M':
+                    current=1000;
+                    break;
+            }
+            if(current<prev){
+                total-=current;
+            }else{
+                total+=current;
+            }
+            prev=current;
+        }
+        return total;
+    }
+}
